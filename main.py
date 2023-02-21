86909bf4b016f6c8e98075cfe730ee0e73c2a0d2
jonatan8254 <josanchezv@unal.edu.co>
2023-02-21 00:42:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 class Asiento:
-    def __init__(self):
-        self.color = ""
-        self.precio = 0
-        self.registro = 0
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
     def cambiarColor(self, color):
-        self.color = color.lower()
+        colores=["rojo", "verde", "amarilo", "negro", "blanco"]
+        if color in colores:
+            self.color = color
         
 
 class Motor:
-    def __init__(self):
-        self.numeroCilindros = 0
-        self.tipo = ""
-        self.registro = 0
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
 
     def cambiarRegistro(self, registro):
         self.registro = registro
@@ -25,19 +27,19 @@ class Motor:
 class Auto:
     cantidadCreados = 0
 
-    def __init__(self, marca, modelo, precio):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = [Asiento() for i in range(5)]
+        self.asientos = asientos
         self.marca = marca
-        self.motor = Motor()
-        self.registro = 0
+        self.motor = Motor
+        self.registro = registro
         Auto.cantidadCreados += 1
 
     def cantidadAsientos(self):
         numAsientos = 0
         for asiento in self.asientos:
-            if asiento.color != "":
+            if isinstance(asiento, Asiento):
                 numAsientos += 1
         return numAsientos
 
